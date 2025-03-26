ba7bce8d3b828d2f3279852f5fcd13bf1820cafd
Urriki1502 <162285992+Urriki1502@users.noreply.github.com>
2025-03-26 23:17:23 +0700

    Delete core directory

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Core package for Quantum Memecoin Trading Bot.
-This package contains the core components that manage the state, configuration,
-security, and adaptability of the trading bot.
-"""
